da73ab7da198387e7ec003618065e7f002852f10
Jon Dawson <chips@jondawson.org.uk>
2010-11-03 18:45:47 +0000

    In example 1 synchonise to the next line of test when reading com port

--- example_1_hello_world.py
+++ example_1_hello_world.py
@@ -26,12 +26,12 @@ def make_system(string, output):
 
 if "simulate" in sys.argv:
     #write the system to the code generator plugin
-    system=make_system("hello byd!\n", Console)
+    system=make_system("helo byd!\n", Console)
     system.test("Example 1: Hello World .... in welsh!", stop_cycles=100)
 
 if "simulate_vhdl" in sys.argv:
     #simulate using an external vhdl simulator
-    system=make_system("hello byd!\n", Console)
+    system=make_system("helo byd!\n", Console)
     vhdl_plugin = streams_VHDL.Plugin()
     system.write_code(vhdl_plugin)
     vhdl_plugin.ghdl_test("Example 1 : Hello world .... in welsh!", stop_cycles=2000)
@@ -40,7 +40,7 @@ if "build" in sys.argv:
     import os
     import shutil
     #compile into a xilinx device
-    system=make_system("hello byd!\n", SerialOut)
+    system=make_system("helo byd!\n", SerialOut)
     vhdl_plugin = streams_VHDL.Plugin(internal_reset=False, internal_clock=False)
     system.write_code(vhdl_plugin)
     from_file=os.path.join(".", "ucfs", "example_1.ucf")
@@ -52,6 +52,7 @@ if "test" in sys.argv:
     #capture output from serial port
     from serial import Serial
     port = Serial('/dev/ttyS0', baudrate=115200, bytesize=8, parity="N", stopbits=1)
+    response = port.readline()
     response = port.readline()
     print response
     port.close()
