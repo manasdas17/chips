e2555add5a9e6c38450749f701bfafe478ceb26e
Jon Dawson <chips@jondawson.org.uk>
2011-05-26 17:44:27 +0100

    updated to version 0.1.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name="Chips",
-      version="0.1.1",
+      version="0.1.2",
       description="Design hardware with Python",
       long_description="""\
     
